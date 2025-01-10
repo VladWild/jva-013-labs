da1bac5d4c59c433cbdfeb595656546fd2ea883c
VladWild <33370139+VladWild@users.noreply.github.com>
2025-01-11 01:17:43 +0500

    Добавил модуль lab-02-start (убрал старые модули 2-ой лабораторной)

--- lab-02-solution/src/main/java/com/luxoft/spingsecurity/repository/CompanyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.luxoft.spingsecurity.repository;
-
-import com.luxoft.spingsecurity.model.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyRepository extends JpaRepository<Company, Long> {
-}
--- lab-02-solution/src/main/java/com/luxoft/spingsecurity/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.luxoft.spingsecurity.repository;
-
-import com.luxoft.spingsecurity.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
