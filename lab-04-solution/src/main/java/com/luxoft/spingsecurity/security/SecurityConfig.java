a1177596967847864d7eaa81333cffba374532c8
VladWild <33370139+VladWild@users.noreply.github.com>
2025-01-11 23:15:27 +0500

    lab-04-solution - сделал 3-ий и 4-ый пункты

--- lab-04-solution/src/main/java/com/luxoft/spingsecurity/security/SecurityConfig.java
+++ lab-04-solution/src/main/java/com/luxoft/spingsecurity/security/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(AbstractHttpConfigurer::disable)
+                //.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(withDefaults())
                 .authorizeHttpRequests(authorize ->
                         authorize
