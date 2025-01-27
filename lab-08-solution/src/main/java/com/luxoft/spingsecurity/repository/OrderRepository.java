d7ab0636ef840438c998eb90210d7f2fc08ccbdd
VladWild <33370139+VladWild@users.noreply.github.com>
2025-01-27 20:13:52 +0300

    lab-08-solution - полностью удалил старое решение

--- lab-08-solution/src/main/java/com/luxoft/spingsecurity/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.luxoft.spingsecurity.repository;
-
-import com.luxoft.spingsecurity.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
